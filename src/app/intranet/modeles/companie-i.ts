6f686573b9eec54cfa4ebeeb98369aee66943761
salimdrch <salim.drch@gmail.com>
2022-11-17 15:47:07 +0100

    mis en place des interfaces compagnie-it

--- src/app/intranet/modeles/companie-i.ts
+++ src/app/intranet/modeles/companie-i.ts
@@ -1,2 +1,28 @@
-export interface CompanieI {
+export interface AvionI{
+    type:string;
+    capacite:number;
+    autonomie?:number;
+    code:string | number;
+}
+
+export interface PersonnelI{
+    nom:string;
+    prenom:Array<string>;
+    habilitation:HabilitationE;
+}
+
+export interface VolI{
+    code:string | number;
+    avion:AvionI;
+    date:Date;
+    personnel:Array<PersonnelI>;
+    aeroportDepart:string;
+    aeroportArrivee:string;
+    duree:number;
+}
+
+enum HabilitationE{
+    pilote = 'Pilote',
+    copilote = 'Copilote',
+    pnc = 'PNC'
 }
